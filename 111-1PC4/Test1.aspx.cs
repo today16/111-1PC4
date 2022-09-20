5929d61fd4ec6060ece11bd922f1ecf056a6c4cf
today16 <k101620275@yahoo.com.tw>
2022-09-20 20:33:55 +0800

    ok

---
+++ 111-1PC4/Test1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC4
+{
+    public partial class Test1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Age = 49;
+            int i_Sex = 0;
+            if (i_Age >= 50)
+            {
+                if (i_Sex == 1)
+                {
+                    Response.Write("壯年男子");
+                }
+                else 
+                {
+                    Response.Write("壯年女子");
+                }
+            }
+            else
+            {
+                if (i_Sex == 1)
+                {
+                    Response.Write("年輕男子");
+                }
+                else
+                {
+                    Response.Write("年輕女子");
+                }
+            }
+        }
+    }
+}
---
+++ 111-1PC4/Test2.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC4
+{
+    public partial class Test2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Age = 2;
+            int i_Sex = 0;
+            if (!(!(i_Age >= 50) || !(i_Sex == 1)))
+            {
+                Response.Write("壯年男子");
+            }
+            else if (!(!(i_Age < 50) || !(i_Sex == 1)))
+            {
+                Response.Write("年輕男子");
+            }
+            else if (!(!(i_Age>= 50) || !(i_Sex == 0)))
+            {
+                Response.Write("壯年女子");
+            }
+            else if (!(!(i_Age < 50) || !(i_Sex == 0)))
+            {
+                Response.Write("年輕女子");
+            }
+            else
+            {
+                Response.Write("其他");
+            }
+        }
+    }
+}
